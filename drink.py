5766ad8e7e67d2c9b133318bd080a81145858669
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-12-08 18:40:31 +0900

    s_ball and drink wav

--- drink.py
+++ drink.py
@@ -3,12 +3,16 @@ import game_world
 import random
 
 class Drink:
+    bnd_sound = None
+
     def __init__(self):
         self.x, self.y = random.randint(100, 1100), random.randint(10, 590)
         # self.frame = random.randint(0, 7)
         # self.speed = random.uniform(3, 5)
         self.image = load_image('drink.png')
         self.removed = False
+        Drink.bnd_sound = load_wav('bnd.wav')
+        Drink.bnd_sound.set_volume(30)
 
     def update(self):
         # self.y -= self.speed
@@ -26,6 +30,7 @@ class Drink:
         if self.removed:
             return
         if group == 'boy:drink':
+            Drink.bnd_sound.play()
             self.removed = True
             game_world.remove_object(self)
 
--- s_ball.py
+++ s_ball.py
@@ -3,12 +3,16 @@ import game_world
 import random
 
 class s_Ball:
+    bnd_sound = None
+
     def __init__(self):
         self.x, self.y = random.randint(100, 1100), random.randint(10, 590)
         # self.frame = random.randint(0, 7)
         # self.speed = random.uniform(3, 5)
         self.image = load_image('ball21x21.png')
         self.removed = False
+        s_Ball.bnd_sound = load_wav('bnd.wav')
+        s_Ball.bnd_sound.set_volume(30)
 
     def update(self):
         # self.y -= self.speed
@@ -26,6 +30,7 @@ class s_Ball:
         if self.removed:
             return
         if group == 'boy:small_ball':
+            s_Ball.bnd_sound.play()
             self.removed = True
             game_world.remove_object(self)
 
